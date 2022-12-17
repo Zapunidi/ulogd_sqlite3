e9118d9f7c9799ba63eb1639671ec36143960e6f
Sergey Zapunidi <zapunidy@gmail.com>
2022-12-17 11:07:35 +0300

    Split to main and ip page

--- ulogd_sqlite3/srv.py
+++ ulogd_sqlite3/srv.py
@@ -12,6 +12,132 @@ from urllib.parse import urlparse, parse_qs
 
 db_filename = ""
 
+
+def int2ip(i):
+    i = struct.unpack("<I", struct.pack(">I", i))[0]
+    return str(ipaddress.IPv4Address(i))
+
+
+def ip2int(ip):
+    o = list(map(int, ip.split('.')))
+    res = (16777216 * o[3]) + (65536 * o[2]) + (256 * o[1]) + o[0]
+    return res
+
+
+def get_ip_page(ip):
+    answer = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>ulogd_sqlite3 main page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <style>
+    .tree{
+      --spacing : 1.5rem;
+      --radius  : 10px;
+    }
+
+    .tree li{
+      display      : block;
+      position     : relative;
+      padding-left : calc(2 * var(--spacing) - var(--radius) - 2px);
+    }
+
+    .tree ul{
+      margin-left  : calc(var(--radius) - var(--spacing));
+      padding-left : 0;
+    }
+
+    .tree ul li{
+      border-left : 2px solid #ddd;
+    }
+
+    .tree ul li:last-child{
+      border-color : transparent;
+    }
+
+    .tree ul li::before{
+      content      : '';
+      display      : block;
+      position     : absolute;
+      top          : calc(var(--spacing) / -2);
+      left         : -2px;
+      width        : calc(var(--spacing) + 2px);
+      height       : calc(var(--spacing) + 1px);
+      border       : solid #ddd;
+      border-width : 0 0 2px 2px;
+    }
+
+    .tree summary{
+      display : block;
+      cursor  : pointer;
+    }
+
+    .tree summary::marker,
+    .tree summary::-webkit-details-marker{
+      display : none;
+    }
+
+    .tree summary:focus{
+      outline : none;
+    }
+
+    .tree summary:focus-visible{
+      outline : 1px dotted #000;
+    }
+
+    .tree li::after,
+    .tree summary::before{
+      content       : '';
+      display       : block;
+      position      : absolute;
+      top           : calc(var(--spacing) / 2 - var(--radius));
+      left          : calc(var(--spacing) - var(--radius) - 1px);
+      width         : calc(2 * var(--radius));
+      height        : calc(2 * var(--radius));
+      border-radius : 50%;
+      background    : #ddd;
+    }
+
+    .tree summary::before{
+      content     : '+';
+      z-index     : 1;
+      background  : #696;
+      color       : #fff;
+      line-height : calc(2 * var(--radius) - 2px);
+      text-align  : center;
+    }
+
+    .tree details[open] > summary::before{
+      content : '−';
+    }
+    </style>
+    </head>
+    <body>"""
+    answer += "<h2>Connections for IP {}</h2>".format(ip)
+
+    answer += """
+    <ul class="tree">"""
+
+    con = sqlite3.connect(db_filename)
+    cur = con.cursor()
+    cur.execute(
+        "SELECT flow_start_sec, flow_end_sec, orig_ip_daddr FROM ulog_ct WHERE orig_ip_saddr = {}".format(ip2int(ip)))
+    for data in cur.fetchall():
+        try:
+            answer += "<li>Destination {}. Duration {}</li>".format(int2ip(data[2]), data[1] - data[0])
+        except Exception:
+            pass
+
+    answer += """</ul>
+
+    </body>
+    </html>"""
+
+    return answer
+
+
 def get_main_page():
     answer = """
 <!DOCTYPE html>
@@ -109,26 +235,13 @@ def get_main_page():
     answer += """
 <ul class="tree">"""
 
-    def int2ip(i):
-        i = struct.unpack("<I", struct.pack(">I", i))[0]
-        return str(ipaddress.IPv4Address(i))
-
     con = sqlite3.connect(db_filename)
     cur = con.cursor()
-    cur.execute("SELECT DISTINCT orig_ip_saddr FROM ulog_ct LIMIT 100")
+    cur.execute("SELECT DISTINCT orig_ip_saddr FROM ulog_ct ORDER BY orig_ip_saddr LIMIT 100")
     iplist = cur.fetchall()
     for ip in iplist:
-        answer += "<li><details><summary>{}</summary><ul>".format(int2ip(ip[0]))
-        cur.execute("SELECT flow_start_sec, flow_end_sec, orig_ip_daddr FROM ulog_ct WHERE orig_ip_saddr = {}".format(ip[0]))
-        for data in cur.fetchall():
-            try:
-                answer += "<li>Destination {}. Duration {}</li>".format(int2ip(data[2]), data[1] - data[0])
-            except Exception:
-                pass
-        answer += "</ul></details></li>"
-
-
-    answer +="""</ul>
+        answer += "<li><a href='/?ip={}'>{}</a></li>".format(int2ip(ip[0]), int2ip(ip[0]))
+    answer += """</ul>
 
 </body>
 </html>"""
@@ -147,7 +260,11 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def _reply_page_and_headers(self, path, query):
         if path == "/" or path == "":
-            answer = get_main_page()
+            if "ip" in query:
+                if query["ip"][0] != "":
+                    answer = get_ip_page(query["ip"][0])
+            else:
+                answer = get_main_page()
         else:
             raise ValueError("Path not found.")
 
