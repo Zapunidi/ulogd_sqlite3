bd743c56bf2c68623dcf46470ab6a4895b641837
Sergey Zapunidi <zapunidy@gmail.com>
2022-12-18 13:38:19 +0300

    Added test for ip conversion

--- tests/test_srv.py
+++ tests/test_srv.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from ulogd_sqlite3.srv import HTTPRequestHandler
+from ulogd_sqlite3.srv import ip2int, int2ip
 
 
 class TestGET(TestCase):
@@ -8,3 +9,9 @@ class TestGET(TestCase):
 
     def test_post(self):
         self.assertTrue("do_POST" in dir(HTTPRequestHandler))
+
+
+class TestIPConvert(TestCase):
+    def test_cycle(self):
+        test_ip = "1.2.3.4"
+        self.assertTrue(test_ip == int2ip(ip2int(test_ip)))
